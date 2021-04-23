afb6753ba9a7dd6f722b41093720fee9aaf45287
matbeyc <matbeyc@mail.ru>
2021-04-23 10:09:32 +1000

    MyFirstCommit

---
+++ test.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+describe('first test in puppeteer',function(){
+    it('launch firs browser',async function(){
+        this.timeout(10000);
+        const browser = await puppeteer.launch({
+                       timeout: 10000,
+                       headless: false,
+                       defaultViewport: {
+                           width: 1200,
+                           height: 1000
+                     }
+                });
+            
+        const page = await browser.newPage();
+        await page.goto('https://www.vl.ru/eda/checkout', {waitUntil: 'load'});
+        await browser.close();
+    });
+});
