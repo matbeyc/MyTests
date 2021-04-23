b7de2eb98839f34641b0f82139f3a94533d740b7
matbeyc <matbeyc@mail.ru>
2021-04-23 10:36:54 +1000

    back height to 1000

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ describe('first test in puppeteer',function(){
                        headless: false,
                        defaultViewport: {
                            width: 1200,
-                           height: 500
+                           height: 1000
                      }
                 });
             
